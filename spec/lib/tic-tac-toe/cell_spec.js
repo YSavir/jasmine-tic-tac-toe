de2c0c4297ee2cdae9546075db6d0140a76ec6ab
Yaniv Savir <saviry@gmail.com>
2015-07-26 15:26:19 -0400

    cells

--- spec/lib/tic-tac-toe/cell_spec.js
+++ spec/lib/tic-tac-toe/cell_spec.js
@@ -1,7 +1,12 @@
-var path = require('path'),
-    TicTacToe = require(path.normalize(GLOBAL.rootPath + 'lib/tic-tac-toe.js'));
+var TicTacToe = require(GLOBAL.rootPath + 'lib/tic-tac-toe.js');
 
 describe('TicTacToe.Cell', function(){
+  it('should initialize with no value', function(){
+    var cell = new TicTacToe.Cell(1, 1);
+  
+    expect(cell.value).toBe(null);
+  });
+
   describe('.coordinates', function(){
     it('should return the cell\'s coordinates', function(){
       var cell1 = new TicTacToe.Cell(1, 1);
@@ -9,6 +14,15 @@ describe('TicTacToe.Cell', function(){
 
       expect(cell1.coordinates()).toEqual([1, 1]);
       expect(cell2.coordinates()).toEqual([2, 1]);
+    });
+  });
+
+  describe('.value', function(){
+    it('should return the cell\'s value', function(){
+      var cell = new TicTacToe.Cell(1, 1);
+      cell.value = 'x';
+
+      expect(cell.value).toEqual('x');
     });
   });
 });
