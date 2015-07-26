f8f8711f0ede35c1be20624958c27c9bc6557a32
Yaniv Savir <saviry@gmail.com>
2015-07-26 14:18:41 -0400

    cells have coordinates

--- spec/lib/tic-tac-toe/cell_spec.js
+++ spec/lib/tic-tac-toe/cell_spec.js
@@ -4,9 +4,11 @@ var path = require('path'),
 describe('TicTacToe.Cell', function(){
   describe('.coordinates', function(){
     it('should return the cell\'s coordinates', function(){
-      var cell = new TicTacToe.Cell(1, 1);
+      var cell1 = new TicTacToe.Cell(1, 1);
+      var cell2 = new TicTacToe.Cell(2, 1);
 
-      expect(cell.coordinates()).toBe([1, 2]);
+      expect(cell1.coordinates()).toEqual([1, 1]);
+      expect(cell2.coordinates()).toEqual([2, 1]);
     });
   });
 });
