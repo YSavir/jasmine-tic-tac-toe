f11de1c89d18f725566b7765faad3bd7fe83c345
Yaniv Savir <saviry@gmail.com>
2015-07-27 11:58:26 -0400

    new TicTacToe.Game

--- spec/lib/tic-tac-toe/game_spec.js
+++ spec/lib/tic-tac-toe/game_spec.js
@@ -1,5 +1,29 @@
 var TicTacToe = require(GLOBAL.rootPath + 'lib/tic-tac-toe.js');
 
 describe('TicTacToe.Game', function(){
-  describe
+  describe('When initialized', function(){
+    it('should have a game interface', function(){
+      var game = new TicTacToe.Game();
+
+      expect(game.interface.constructor).toBe(TicTacToe.Interfaces.Game);
+    });
+
+    it('should have an array of players', function(){
+      var game = new TicTacToe.Game();
+
+      expect(Array.isArray(game.players)).toBeTruthy();
+    });
+
+    it('should not be complete', function(){
+      var game = new TicTacToe.Game();
+
+      expect(game.completed).toBe(false);
+    });
+
+    it('should have a grid', function(){
+      var game = new TicTacToe.Game();
+
+      expect(game.grid.constructor).toBe(TicTacToe.Grid);
+    });
+  });
 });
