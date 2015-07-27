7aadfc89d1fd2e06d4379d302f49e9e75f277898
Yaniv Savir <saviry@gmail.com>
2015-07-27 10:30:40 -0400

    grid.cellsWithValues

--- spec/lib/tic-tac-toe/grid_spec.js
+++ spec/lib/tic-tac-toe/grid_spec.js
@@ -12,7 +12,7 @@ describe('TicTacToe.Grid', function(){
       var grid = new TicTacToe.Grid();
       var cellCoordinates = grid.cells.map(function(cell){
         return cell.coordinates();
-      })
+      });
       var expectedCoordinates = [[1, 1], [1, 2], [1, 3],
                                  [2, 1], [2, 2], [2, 3],
                                  [3, 1], [3, 2], [3, 3]];
@@ -29,6 +29,34 @@ describe('TicTacToe.Grid', function(){
 
       expect(grid.cell(1, 1)).toEqual(cell1);
       expect(grid.cell(2, 2)).toEqual(cell2);
+    });
+  });
+
+  describe('.cellsWithValues', function(){
+    describe('Given a grid with 2 cells with values', function(){
+      it('should return the 2 cells with values', function(){
+        var grid = new TicTacToe.Grid(),
+            cell1 = grid.cell(1, 1),
+            cell2 = grid.cell(2, 2);
+        cell1.value = 'X';
+        cell2.value = 'X';
+
+        expect(grid.cellsWithValues()).toEqual([cell1, cell2]);
+      });
+    });
+
+    describe('Given a grid with 3 cells with values', function(){
+      it('should return the 3 cells with values', function(){
+        var grid = new TicTacToe.Grid(),
+            cell1 = grid.cell(1, 1),
+            cell2 = grid.cell(2, 2),
+            cell3 = grid.cell(3, 3);
+        cell1.value = 'X';
+        cell2.value = 'X';
+        cell3.value = 'X';
+
+        expect(grid.cellsWithValues()).toEqual([cell1, cell2, cell3]);
+      }); 
     });
   });
 });
