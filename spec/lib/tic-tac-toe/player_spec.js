b2a915015f02b232bd47333827e831054b76c915
Yaniv Savir <saviry@gmail.com>
2015-07-27 12:45:56 -0400

    new TicTacToe.Player()

---
+++ spec/lib/tic-tac-toe/player_spec.js
@@ -0,0 +1,38 @@
+var TicTacToe = require(GLOBAL.rootPath + 'lib/tic-tac-toe.js');
+
+describe('TicTacToe.Player()', function(){
+  describe('When initialized', function(){
+    describe('as a computer player', function(){
+      it('should be human', function(){
+        var player = new TicTacToe.Player({human: true});
+
+        expect(player.human).toBe(true);
+      });
+    });
+
+    describe('with no human option', function(){
+      it('should not be human', function(){
+        var player = new TicTacToe.Player();
+        console.log(player.human);
+
+        expect(player.human).toBe(false);
+      });
+    });
+
+    describe('with \'X\' as a symbol', function(){
+      it('should have \'X\' as a symbol', function(){
+        var player = new TicTacToe.Player({symbol: 'X'});
+
+        expect(player.symbol).toBe('X');
+      });
+    });
+
+    describe('with \'O\' as a symbol', function(){
+      it('should ahve \'O\' as a symbol', function(){
+        var player = new TicTacToe.Player({symbol: 'O'});
+
+        expect(player.symbol).toBe('O');
+      });
+    });
+  });
+});
